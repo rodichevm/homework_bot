ff2d1f761ef53e9fe6a91d61096b559a18b462a3
Michael <rodichevmichael@gmail.com>
2023-03-08 22:24:43 +0300

    moving to another pc

--- homework.py
+++ homework.py
@@ -27,47 +27,37 @@ HOMEWORK_VERDICTS = {
 }
 
 SUCCESSFUL_SENT_MESSAGE = 'Сообщение {message} успешно отправлено'
-UNSUCCESSFUL_SENT_MESSAGE = 'Не удалось отправить сообщение "{message}"'
+UNSUCCESSFUL_SENT_MESSAGE = (
+    'Не удалось отправить сообщение "{message}. Ошибка:{error}"')
 STATUS_MESSAGE = 'Изменился статус проверки работы "{homework_name}".{verdict}'
 UNEXPECTED_STATUS = 'Неожиданный статус домашней работы:"{status}"'
-UNEXPECTED_TYPE = 'Неожиданный тип содержимого:{type}'
+UNEXPECTED_TYPE_DICT = 'Вместо типа dict получен:{type}'
+UNEXPECTED_TYPE_LIST = 'Вместо типа list получен: {type}'
 UNEXPECTED_API_RESPONSE = (
     'Ответ API не соответствует документации({response}).{error}')
 API_FAILED_RESPONSE = (
     'Эндпоинт API {ENDPOINT} недоступен. Код ответа API: {status_code}. '
-    'Header: {HEADERS}. Timestamp: {timestamp}'
+    'Header: {HEADERS}'
 )
 API_FAILED_REQUEST = (
     'Ошибка запроса к API:{error}, Эндпоинт API: {ENDPOINT}, '
     'Header: {HEADERS}, Timestamp: {timestamp}'
 )
 NO_KEY = 'Отсутствует ключ {key}'
-NO_VARIABLE = 'Отсутствует обязательная переменная окружения'
+NO_VARIABLE = 'Отсутствует обязательная переменная окружения {token}'
 ERROR_GLOBAL = 'Ошибка в работе бота: {error}'
 
-
-logging.basicConfig(
-    level=logging.INFO,
-    filename=__file__ + '.log',
-    filemode='a'
-)
 logger = logging.getLogger(__name__)
-handler = logging.StreamHandler(stream=sys.stdout)
-logger.addHandler(handler)
-formatter = logging.Formatter(
-    '%(asctime)s, %(funcName)s, %(levelname)s, %(message)s'
-)
-handler.setFormatter(formatter)
 
 
 def check_tokens():
     """Проверяем доступность переменных окружения."""
-    tokens = [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]
-    for token in tokens:
-        if token is None:
-            logger.critical(NO_VARIABLE)
-            raise ValueError(NO_VARIABLE)
-        return all({PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID})
+    # tokens = [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]
+    # for token in tokens:
+    #     if token is None:
+    #         logger.critical(NO_VARIABLE.format(token=token))
+    #     raise ValueError(NO_VARIABLE.format(token=token))
+    return all({PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID})
 
 
 def send_message(bot, message):
@@ -75,86 +65,48 @@ def send_message(bot, message):
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.debug(SUCCESSFUL_SENT_MESSAGE.format(message=message))
-    except telegram.TelegramError:
-        logger.exception(UNSUCCESSFUL_SENT_MESSAGE.format(message=message))
+    except telegram.TelegramError as error:
+        logger.exception(UNSUCCESSFUL_SENT_MESSAGE.format(
+            message=message, error=error))
 
 
 def get_api_answer(timestamp):
     """Делает запрос к эндпоинту API."""
-    try:
-        response = requests.get(
-            ENDPOINT,
-            headers=HEADERS,
-            params={'from_date': timestamp}
+    rq_pars = dict(
+        url=ENDPOINT, headers=HEADERS, params={'from_date': timestamp})
+    response = requests.get(**rq_pars)
+    if response.status_code != HTTPStatus.OK:
+        raise APIHTTPRequestError(API_FAILED_RESPONSE.format(
+            status_code=response.status_code, **rq_pars)
         )
-        if response.status_code != HTTPStatus.OK:
-            raise APIHTTPRequestError(API_FAILED_RESPONSE.format(
-                ENDPOINT=ENDPOINT,
-                HEADERS=HEADERS,
-                status_code=response.status_code,
-                timestamp=timestamp)
-            )
-        if not response.json():
-            logger.exception(API_FAILED_RESPONSE.format(
-                ENDPOINT=ENDPOINT,
-                HEADERS=HEADERS,
-                status_code=response.status_code,
-                timestamp=timestamp)
-            )
-            raise ServerError(API_FAILED_RESPONSE.format(
-                ENDPOINT=ENDPOINT,
-                HEADERS=HEADERS,
-                status_code=response.status_code,
-                timestamp=timestamp)
-            )
+    try:
         return response.json()
     except requests.RequestException as error:
-        logger.exception(API_FAILED_REQUEST.format(
-            error=error,
-            ENDPOINT=ENDPOINT,
-            HEADERS=HEADERS,
-            timestamp=timestamp)
-        )
-        raise ServerError(API_FAILED_REQUEST.format(
-            error=error,
-            ENDPOINT=ENDPOINT,
-            HEADERS=HEADERS,
-            timestamp=timestamp)
-        )
+        raise Exception(API_FAILED_REQUEST.format(error=error, **rq_pars))
 
 
 def check_response(response):
     """Проверяет ответ API на соответствие документации."""
-    try:
-        if not isinstance(response, dict):
-            raise TypeError(UNEXPECTED_TYPE.format(type=type(response)))
-        if 'homeworks' not in response.keys():
-            raise KeyError(NO_KEY.format(key='homeworks'))
-        if type(response['homeworks']) is not list:
-            raise TypeError(
-                UNEXPECTED_TYPE.format(type=type(response['homeworks'])))
-        return response.get('homeworks')
-    except TypeError as error:
-        logger.exception(error)
-        raise TypeError(UNEXPECTED_API_RESPONSE.format(
-            response=response, error=error))
+    if not isinstance(response, dict):
+        raise TypeError(UNEXPECTED_TYPE_DICT.format(type=type(response)))
+    if 'homeworks' not in response.keys():
+        raise KeyError(NO_KEY.format(key='homeworks'))
+    if not isinstance(response['homeworks'], list):
+        raise TypeError(
+            UNEXPECTED_TYPE_LIST.format(type=type(response['homeworks'])))
+    return response.get('homeworks')
 
 
 def parse_status(homework):
     """Возвращает статус домашней работы."""
-    try:
-        if not homework.get('homework_name'):
-            raise ValueError(NO_KEY.format(key='homework_name'))
-        status = homework.get('status')
-        if status not in HOMEWORK_VERDICTS.keys():
-            raise ValueError(UNEXPECTED_STATUS.format(status=status))
-        homework_name = homework.get('homework_name')
-        verdict = HOMEWORK_VERDICTS[status]
-        return STATUS_MESSAGE.format(
-            homework_name=homework_name, verdict=verdict)
-    except Exception as error:
-        logger.exception(error)
-        raise ValueError(error)
+    if 'homework_name' not in homework:
+        raise ValueError(NO_KEY.format(key='homework_name'))
+    status = homework.get('status')
+    if status not in HOMEWORK_VERDICTS:
+        raise ValueError(UNEXPECTED_STATUS.format(status=status))
+    return STATUS_MESSAGE.format(
+        homework_name=homework.get(
+            'homework_name'), verdict=HOMEWORK_VERDICTS[status])
 
 
 def main():
@@ -163,13 +115,12 @@ def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     while True:
-        api_answer = get_api_answer(timestamp)
-        homeworks = check_response(api_answer)
         try:
+            api_answer = get_api_answer(timestamp)
+            homeworks = check_response(api_answer)
             if homeworks:
                 message = parse_status(homeworks[0])
                 send_message(bot, message)
-                logger.info(SUCCESSFUL_SENT_MESSAGE.format(message=message))
         except Exception as error:
             send_message(bot, ERROR_GLOBAL.format(error=error))
             logger.exception(error)
@@ -178,9 +129,16 @@ def main():
 
 if __name__ == '__main__':
     try:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            filename=__file__ + '.log',
+            format='%(asctime)s, %(funcName)s, %(levelname)s, %(message)s'
+        )
+        handler = logging.StreamHandler(sys.stdout)
+        logger.addHandler(handler)
         main()
     except KeyboardInterrupt as error:
-        logger.exception(error)
+        logger.exception(f'Программа была остановлена:{error}')
 
     # for testing
     # from unittest import TestCase, mock, main as uni_main
