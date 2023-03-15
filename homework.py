a02995d140431f4984054bf6c367ffe8c09a6e00
Michael <rodichevmichael@gmail.com>
2023-03-15 11:39:07 +0300

    5th review

--- homework.py
+++ homework.py
@@ -8,8 +8,6 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-from exceptions import APIHTTPRequestError
-
 load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
@@ -32,7 +30,8 @@ UNSUCCESSFUL_SENT_MESSAGE = (
     'Не удалось отправить сообщение "{message}. Ошибка:{error}"')
 STATUS_MESSAGE = 'Изменился статус проверки работы "{homework_name}".{verdict}'
 UNEXPECTED_STATUS = 'Неожиданный статус домашней работы:"{status}"'
-UNEXPECTED_TYPE = 'Получен неожиданный тип: {type}'
+UNEXPECTED_TYPE_LIST = 'Ответ API не вернул список(list), а вернул тип: {type}'
+UNEXPECTED_TYPE_DICT = 'Ответ API не вернул список(dict), а вернул тип: {type}'
 UNEXPECTED_API_RESPONSE = (
     'Ответ API не соответствует документации({response}).{error}')
 API_FAILED_RESPONSE = (
@@ -44,6 +43,7 @@ API_FAILED_REQUEST = (
     'Header: {HEADERS}, Timestamp: {timestamp}. Ошибка: {error}'
 )
 NO_KEY = 'Отсутствует ключ {key}'
+NO_KEY_HOMEWORK_NAME = 'Отсутствует ключ домашней работы "homework_name"'
 NO_VARIABLE = 'Отсутствует обязательная переменная окружения {token}'
 ERROR_GLOBAL = 'Ошибка в работе бота: {error}'
 
@@ -53,9 +53,9 @@ logger = logging.getLogger(__name__)
 def check_tokens():
     """Проверяем доступность переменных окружения."""
     missed_tokens = [token for token in TOKENS if not globals().get(token)]
-    for token in missed_tokens:
-        logger.critical(NO_VARIABLE.format(token=token))
-        raise ValueError(NO_VARIABLE.format(token=token))
+    if missed_tokens:
+        logger.critical(NO_VARIABLE.format(token=missed_tokens))
+        raise ValueError(NO_VARIABLE.format(token=missed_tokens))
 
 
 def send_message(bot, message):
@@ -70,20 +70,19 @@ def send_message(bot, message):
 
 def get_api_answer(timestamp):
     """Делает запрос к эндпоинту API."""
-    keys = ['code', 'error']
     request_params = dict(
         url=ENDPOINT, headers=HEADERS, params={'from_date': timestamp})
     try:
         response = requests.get(**request_params)
     except requests.RequestException as error:
-        raise APIHTTPRequestError(
+        raise ValueError(
             API_FAILED_REQUEST.format(error=error, **request_params))
     if response.status_code != HTTPStatus.OK:
-        raise Exception(API_FAILED_RESPONSE.format(
+        raise ValueError(API_FAILED_RESPONSE.format(
             status_code=response.status_code, **request_params)
         )
     api_response = response.json()
-    for key in keys:
+    for key in ['code', 'error']:
         if key in api_response:
             raise ValueError(
                 API_FAILED_RESPONSE.format(
@@ -94,20 +93,20 @@ def get_api_answer(timestamp):
 def check_response(response):
     """Проверяет ответ API на соответствие документации."""
     if not isinstance(response, dict):
-        raise TypeError(UNEXPECTED_TYPE.format(type=type(response)))
+        raise TypeError(UNEXPECTED_TYPE_DICT.format(type=type(response)))
     if 'homeworks' not in response:
         raise KeyError(NO_KEY.format(key='homeworks'))
-    api_homework = response.get('homeworks')
-    if not isinstance(api_homework, list):
+    homeworks = response.get('homeworks')
+    if not isinstance(homeworks, list):
         raise TypeError(
-            UNEXPECTED_TYPE.format(type=type(api_homework)))
-    return api_homework
+            UNEXPECTED_TYPE_LIST.format(type=type(homeworks)))
+    return homeworks
 
 
 def parse_status(homework):
     """Возвращает статус домашней работы."""
     if 'homework_name' not in homework:
-        raise ValueError(NO_KEY.format(key='homework_name'))
+        raise ValueError(NO_KEY_HOMEWORK_NAME)
     status = homework.get('status')
     if status not in HOMEWORK_VERDICTS:
         raise ValueError(UNEXPECTED_STATUS.format(status=status))
@@ -122,22 +121,21 @@ def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     previous_message = ''
-    previous_error = ''
+    message = ''
     while True:
         try:
             api_answer = get_api_answer(timestamp)
             homeworks = check_response(api_answer)
             if homeworks:
                 message = parse_status(homeworks[0])
-                if previous_message != message:
-                    if send_message(bot, message):
-                        previous_message = message
-                        timestamp = api_answer.get('current_date', timestamp)
+            if previous_message != message and send_message(bot, message):
+                previous_message = message
+                timestamp = api_answer.get('current_date', timestamp)
         except Exception as error:
-            if previous_error != error:
-                send_message(bot, ERROR_GLOBAL.format(error=error))
-                previous_error = error
+            message = ERROR_GLOBAL.format(error=error)
             logging.exception(ERROR_GLOBAL.format(error=error))
+            if message != previous_message and send_message(bot, message):
+                previous_message = message
         time.sleep(RETRY_PERIOD)
 
 
